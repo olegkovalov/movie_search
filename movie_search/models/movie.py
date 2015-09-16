1d29e21caec45b4fe1d8aff834732206391618bf
Oleg Kovalyov <oleg.kovalov@masterofcode.com>
2015-09-16 11:58:18 +0300

    added search view; Movie model; templates: base.html, search.html

---
+++ movie_search/models/movie.py
@@ -0,0 +1,27 @@
+import requests
+
+from datetime import datetime
+from mongoengine import Document, StringField, SequenceField, DateTimeField
+
+from movie_search.files import FileManage
+
+
+class Movie(Document):
+    _id = SequenceField(primary_key=True)
+    title = StringField(required=True)
+    poster = StringField()
+    created = DateTimeField(default=datetime.now())
+
+    def update_poster(self, poster_url):
+        r = requests.get(poster_url)
+        filename = self.title.lower() + '.' + r.headers['content-type'].split('/')[1]
+        if not self.poster:
+            f = FileManage(filename)
+            self.poster = f.upload(r.content)
+        else:
+            f = FileManage(self.poster)
+            if int(r.headers['content-length']) != f.get_length():
+                print f.get_length()
+                print(r.headers['content-length'])
+                f.remove()
+                self.poster = f.upload(r)
--- movie_search/views/main.py
+++ movie_search/views/main.py
@@ -1,6 +1,56 @@
-from pyramid.view import view_config
+import requests
+
+from bson import json_util
+from pyramid.view import view_defaults, view_config
+
+from movie_search.models.movie import Movie
+from movie_search.settings import OMDB_URL
 
 
-@view_config(route_name='search', renderer='json')
-def search(request):
-    return {}
+class BaseView(object):
+    def __init__(self, request):
+        self.req = request
+
+
+@view_defaults(route_name='search', renderer='search.html')
+class SearchView(BaseView):
+
+    @view_config(request_method='GET')
+    def get(self):
+        q = self.req.params.get('q')
+        movie = None
+        title = None
+
+        if q and len(q) > 3:
+            title, poster_url = self.get_movie_info(q)
+
+        if title:
+            movie = self.get_or_create_movie(title, poster_url)
+
+        return {
+            'movie': movie,
+            'q': q,
+        }
+
+    def get_movie_info(self, q):
+        title, poster_url = None, None
+        params = dict(t=q, plot='short', r='json')
+        r = requests.get(OMDB_URL, params=params)
+        content = json_util.loads(r.content)
+        if not content.get('Error'):
+            title = content.get('Title')
+            if content.get('Poster') != 'N/A':
+                poster_url = content.get('Poster')
+        return title, poster_url
+
+    def get_or_create_movie(self, title, poster_url):
+        if not Movie.objects(title=title).count():
+            movie = Movie(title=title).save()
+        else:
+            movie = Movie.objects(title=title).first()
+
+        if poster_url:
+            movie.update_poster(poster_url)
+            movie.save()
+        return movie
+
